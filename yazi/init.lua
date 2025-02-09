8642826057e8864ee36d11bc66fc91ea97a73667
martelzhang <1814496909@qq.com>
2025-02-10 01:16:18 +0800

    update

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,4 @@
-require("yaziline"):setup {
-	separator_style = "curvy",
-	select_symbol = "",
-	yank_symbol = "󰆐",
-	filename_max_length = 24, -- trim when filename > 24
-	filename_trim_length = 6 -- trim 6 chars from both ends
-}
+require("yaziline"):setup()
 require("starship"):setup {
 	config_file = "~/.config/yazi/starship.toml",
 }
